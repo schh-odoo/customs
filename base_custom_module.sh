353c8b7e324af1fbf92e2b7ce0632a2970033f05
sankalp chhunchha <124247994+schh-odoo@users.noreply.github.com>
2023-03-03 18:21:41 +0530

    Added functionality: folder,database,run server

--- base_custom_module.sh
+++ base_custom_module.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Decelaration of all the variables
-MODULE_NAME="$1"                                        #MODULE NAME
-MODULE_NAME_CAP="$2"                                    #APP NAME
-MODEL_NAME="$3"                                         #MODEL UNDERSCORE NAME
+FOLDER_NAME="$1"                                        #FOLDER NAME
+MODULE_NAME="$2"                                        #MODULE NAME
+MODULE_NAME_CAP="$3"                                    #APP NAME
+MODEL_NAME="$4"                                         #MODEL UNDERSCORE NAME
+RUN_SERVER="$5"                                         #RUN SERVER
+DB_NAME="$6"                                            #DATABASE NAME
 MODEL_NAME_PY=$MODEL_NAME.py                            #.py NAME
 MODEL_TABLE_NAME=${MODEL_NAME//[_]/.}                   #UNDERSCORE NAME REPLACED WITH .
 arr=(${MODEL_NAME//_/ })                                #STORES THE SEPERATED CAPITALISED INDIVIDUAL NAMES
@@ -24,7 +27,7 @@ MENU_XML_FILE=$MODULE_NAME$MENU.xml                     #MODULE MENU XML FILE NA
 #ROOT MENU
 MENUITEM="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<odoo>\n\t<menuitem id=\"$MODEL_NAME$ROOT\" name=\"$MODULE_NAME_CAP\">\n\t\t<menuitem id=\"$MODEL_NAME$FIRST\" name=\"Home\">\n\t\t\t<menuitem id=\"$MODEL_NAME$THIRD\" name=\"$arr\" action=\"$MODEL_NAME$UNDERSCORE$ACTION\" />\n\t\t</menuitem>\n\t</menuitem>\n</odoo>"
 
-SECURITY_FILE="ir.model.access.csv"                     #SECRITY CSV FILE
+SECURITY_FILE="ir.model.access.csv"                     #SECRITY CSV FILE NAME
 MODEL_INIT_IMPORT="from . import $MODEL_NAME"           #MODEL IMPORT __INIT__.PY FILE
 ROOT_INIT_IMPORT="from . import models"                 #ROOT IMPORT __INIT__.PY FILE
 
@@ -41,47 +44,84 @@ MODEL_SECURITY_CSV="$MODULE_NAME.access_$MODEL_NAME,access_$MODEL_NAME,$MODULE_N
 MODEL_VIEW="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<odoo>\n\t<!-- Blank Record -->\n\t<record id=\"$MODEL_NAME$UNDERSCORE$ACTION\" model=\"ir.actions.act_window\">\n\t\t<field name=\"name\">Home</field>\n\t\t<field name=\"res_model\">$MODEL_TABLE_NAME</field>\n\t\t<field name=\"view_mode\">tree,form</field>\n\t\t<field name=\"help\" type=\"html\">\n\t\t\t<p class=\"o_view_nocontent_smiling_face\">\n\t\t\t\tCreate your first property\n\t\t\t</p>\n\t\t</field>\n\t</record>\n\n\t<!-- Tree View -->\n\t<record id=\"$MODEL_NAME$UNDERSCORE$TREE$UNDERSCORE$VIEW\" model=\"ir.ui.view\">\n\t\t<field name=\"name\">$MODEL_NAME$UNDRESCORE$TREE</field>\n\t\t<field name=\"model\">$MODEL_TABLE_NAME</field>\n\t\t<field name=\"arch\" type=\"xml\">\n\t\t\t<tree>\n\t\t\t\t<field name=\"name\" />\n\t\t\t</tree>\n\t\t</field>\n\t</record>\n</odoo>"
 
 # ----------------------------------------------    FLOW    ----------------------------------------------------- #
-cd odoo/custom
-mkdir $MODULE_NAME
 
-echo "Creating $MODULE_NAME module..."
+if [ "$RUN_SERVER" != "" ] && [ "$RUN_SERVER" == "run" ] && [ "$DB_NAME" != "" ]
+then
+    cd odoo/$FOLDER_NAME
+    mkdir $MODULE_NAME
 
-cd $MODULE_NAME
-touch $INIT_FILE $MODULE_MANIFEST_FILE
-mkdir models static views security data
-echo $ROOT_INIT_IMPORT>>$INIT_FILE
-echo -e $MANIFEST_OBJ>>$MODULE_MANIFEST_FILE
+    echo "Creating $MODULE_NAME module..."
 
-cd models
-touch $INIT_FILE $MODEL_NAME_PY
-echo $MODEL_INIT_IMPORT>>$INIT_FILE
-echo -e $MODEL_OBJ>>$MODEL_NAME_PY
-cd ..
+    cd $MODULE_NAME
+    touch $INIT_FILE $MODULE_MANIFEST_FILE
+    mkdir models static views security data
+    echo $ROOT_INIT_IMPORT>>$INIT_FILE
+    echo -e $MANIFEST_OBJ>>$MODULE_MANIFEST_FILE
 
-cd security
-touch $SECURITY_FILE
-echo "id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink">>$SECURITY_FILE
-echo $MODEL_SECURITY_CSV>>$SECURITY_FILE
-cd ..
+    cd models
+    touch $INIT_FILE $MODEL_NAME_PY
+    echo $MODEL_INIT_IMPORT>>$INIT_FILE
+    echo -e $MODEL_OBJ>>$MODEL_NAME_PY
+    cd ..
 
-cd static
-mkdir description
-cd ..
+    cd security
+    touch $SECURITY_FILE
+    echo "id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink">>$SECURITY_FILE
+    echo $MODEL_SECURITY_CSV>>$SECURITY_FILE
+    cd ..
 
-cd views
-touch $MENU_XML_FILE
-touch $MODEL_VIEW_XML
-echo -e $MENUITEM>>$MENU_XML_FILE
-echo -e $MODEL_VIEW>>$MODEL_VIEW_XML
-cd ..
+    cd static
+    mkdir description
+    cd ..
 
-echo "Created $MODULE_NAME module succesfully"
+    cd views
+    touch $MENU_XML_FILE
+    touch $MODEL_VIEW_XML
+    echo -e $MENUITEM>>$MENU_XML_FILE
+    echo -e $MODEL_VIEW>>$MODEL_VIEW_XML
+    cd ..
+    cd ..
+    cd ..
 
+    echo "Created $MODULE_NAME module succesfully"
+    
+    echo "Running server on localhost"
+    cd community
+    ./odoo-bin --addons-path=addons,../enterprise,../$FOLDER_NAME -d $DB_NAME
+else
+    cd odoo/$FOLDER_NAME
+    mkdir $MODULE_NAME
 
-# --------------------------------------------- Optional  ------------------------------------------------#
-#to run the server with the custom module save:
+    echo "Creating $MODULE_NAME module..."
 
-#uncomment if needed
-#cd ~
-#cd odoo/community
-#./odoo-bin --addons-path=addons,../enterprise,../custom -d "DATABASE NAME"
+    cd $MODULE_NAME
+    touch $INIT_FILE $MODULE_MANIFEST_FILE
+    mkdir models static views security data
+    echo $ROOT_INIT_IMPORT>>$INIT_FILE
+    echo -e $MANIFEST_OBJ>>$MODULE_MANIFEST_FILE
+
+    cd models
+    touch $INIT_FILE $MODEL_NAME_PY
+    echo $MODEL_INIT_IMPORT>>$INIT_FILE
+    echo -e $MODEL_OBJ>>$MODEL_NAME_PY
+    cd ..
+
+    cd security
+    touch $SECURITY_FILE
+    echo "id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink">>$SECURITY_FILE
+    echo $MODEL_SECURITY_CSV>>$SECURITY_FILE
+    cd ..
+
+    cd static
+    mkdir description
+    cd ..
+
+    cd views
+    touch $MENU_XML_FILE
+    touch $MODEL_VIEW_XML
+    echo -e $MENUITEM>>$MENU_XML_FILE
+    echo -e $MODEL_VIEW>>$MODEL_VIEW_XML
+    cd ..
+
+    echo "Created $MODULE_NAME module succesfully"
+fi
